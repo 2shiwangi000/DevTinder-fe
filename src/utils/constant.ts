a43b648889a68d1858705ca65c2712f64ad67b19
2shiwangi000 <2shiwangi000@gmail.com>
2026-02-23 19:04:53 +0530

    const

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,5 +1,2 @@
-//local url
-// export const BASE_URL = "http://localhost:4000/"
-
-//prod
-export const BASE_URL = "/api/"
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:4000/" : "/api/";
